679c0b354eb600e9aba2ff02c5f593b50f85cfe7
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2016-03-22 22:58:38 -0700

    do it in a safer way

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='aio-s3',
-      version='0.5.15',
+      version='0.5.16',
       description='Asyncio-based client for S3',
       author='Paul Colomiets',
       author_email='paul@colomiets.name',
